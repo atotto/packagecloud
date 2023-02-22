625a25d38dcba09cc1f34c76f849202b599a486d
Ato Araki <ato.araki@gmail.com>
2023-02-22 19:57:39 +0900

    fix test

---
+++ api/v1/distributions_export_test.go
@@ -0,0 +1,3 @@
+package packagecloud
+
+var FindDistroVersionID = findDistroVersionID
--- api/v1/distributions_test.go
+++ api/v1/distributions_test.go
@@ -1,10 +1,11 @@
-package packagecloud
+package packagecloud_test
 
 import (
 	"context"
 	"os"
 	"testing"
 
+	packagecloud "github.com/atotto/packagecloud/api/v1"
 	"github.com/atotto/packagecloud/api/v1/packagecloudtest"
 )
 
@@ -12,12 +13,12 @@ func TestDistro(t *testing.T) {
 	ctx := context.Background()
 	ctx = packagecloudtest.SetupToken(t, ctx, os.Getenv("PACKAGECLOUD_TOKEN"))
 
-	distributions, err := GetDistributions(ctx)
+	distributions, err := packagecloud.GetDistributions(ctx)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	id, ok := findDistroVersionID(distributions.Deb, "debian", "stretch")
+	id, ok := packagecloud.FindDistroVersionID(distributions.Deb, "debian", "stretch")
 	if !ok {
 		t.Fatal("not found")
 	}
